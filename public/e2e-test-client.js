243dc07f10745878b0405d80d55de516199861ce
i340818 <will.sun@sap.com>
2020-12-28 16:31:36 +0800

    some update

--- public/e2e-test-client.js
+++ public/e2e-test-client.js
@@ -56,35 +56,12 @@ window.onload = function () {
             }).done(function (result) {
                 if (result && result.pattern == 'HOSTED_FIELDS') {
                     // start to render the hosted fileds with the response result
-
-                    // first add scripts
-                    // if (result.dynamicScript.jsUrls && result.dynamicScript.jsUrls.length > 0) {
-                        
-                    // } else {
-                    //    result.dynamicScript.jsUrls = [
-                    //        {url: 'https://js.braintreegateway.com/web/3.69.0/js/client.min.js'},
-                    //        {url: 'https://js.braintreegateway.com/web/3.69.0/js/data-collector.min.js'},
-                    //        {url: 'https://js.braintreegateway.com/web/3.69.0/js/hosted-fields.min.js'},
-                    //        {url: 'http://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.2.1.min.js'}
-                    //    ]
-                    // }
-
-                    // result.dynamicScript.jsUrls.forEach(js => {
-                    //     var script = document.createElement("script");
-                    //     script.type = "text/javascript";
-                    //     script.src = js.url;
-                    //     document.body.appendChild(script);
-                    // });
-
-                    // then render html
-                    // var ifrdoc = iframe.contentWindow.document;
-                    // ifrdoc.open();
-                    // ifrdoc.write(result.dynamicScript.html);
-                    // ifrdoc.close();
-
                     var htmlSrcUrl = window.URL.createObjectURL(new Blob([result.dynamicScript.html], {type: 'text/html;charset=utf-8'}))
                     console.log(htmlSrcUrl);
                     iframe.src = htmlSrcUrl;
+                    // set local storage
+                    localStorage.setItem("Authorization", jwtToken.value);
+                    localStorage.setItem("Customer-session-id", customerSessionId.value);
                 }
             });
         }
